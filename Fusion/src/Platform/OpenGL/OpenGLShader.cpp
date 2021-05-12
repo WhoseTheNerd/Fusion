6650da8e5fe3535d4130cff304920ce8158311f8
WhoseTheNerd <tutorialvideohd@gmail.com>
2021-05-12 22:35:09 +0300

    Useless std::string; replaced it with const char*

--- Fusion/src/Platform/OpenGL/OpenGLShader.cpp
+++ Fusion/src/Platform/OpenGL/OpenGLShader.cpp
@@ -87,51 +87,51 @@ namespace Fusion { namespace Graphics {
 		glUseProgram(NULL);
 	}
 
-	void OpenGLShader::SetInt(const std::string& name, const int value)
+	void OpenGLShader::SetInt(const char* name, const int value)
 	{
 		int location = GetUniformLocation(name);
 		glProgramUniform1i(m_Program, location, value);
 	}
 
-	void OpenGLShader::SetIntArray(const std::string& name, const int* const values, const uint32_t count)
+	void OpenGLShader::SetIntArray(const char* name, const int* const values, const uint32_t count)
 	{
 		int location = GetUniformLocation(name);
 		glProgramUniform1iv(m_Program, location, count, values);
 	}
 
-	void OpenGLShader::SetFloat(const std::string& name, float value)
+	void OpenGLShader::SetFloat(const char* name, float value)
 	{
 		int location = GetUniformLocation(name);
 		glProgramUniform1f(m_Program, location, value);
 	}
 
-	void OpenGLShader::SetFloat2(const std::string& name, const glm::vec2& value)
+	void OpenGLShader::SetFloat2(const char* name, const glm::vec2& value)
 	{
 		int location = GetUniformLocation(name);
 		glProgramUniform2f(m_Program, location, value.x, value.y);
 	}
 
-	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
+	void OpenGLShader::SetFloat3(const char* name, const glm::vec3& value)
 	{
 		int location = GetUniformLocation(name);
 		glProgramUniform3f(m_Program, location, value.x, value.y, value.z);
 	}
 
-	void OpenGLShader::SetFloat4(const std::string& name, const glm::vec4& value)
+	void OpenGLShader::SetFloat4(const char* name, const glm::vec4& value)
 	{
 		int location = GetUniformLocation(name);
 		glProgramUniform4f(m_Program, location, value.x, value.y, value.z, value.w);
 	}
 
-	void OpenGLShader::SetMat4(const std::string& name, const glm::mat4& value)
+	void OpenGLShader::SetMat4(const char* name, const glm::mat4& value)
 	{
 		int location = GetUniformLocation(name);
 		glProgramUniformMatrix4fv(m_Program, location, 1, GL_FALSE, glm::value_ptr(value));
 	}
 
-	int OpenGLShader::GetUniformLocation(const std::string& name)
+	int OpenGLShader::GetUniformLocation(const char* name)
 	{
-		return glGetUniformLocation(m_Program, name.c_str());
+		return glGetUniformLocation(m_Program, name);
 	}
 
 } }
--- Fusion/src/Platform/OpenGL/OpenGLShader.h
+++ Fusion/src/Platform/OpenGL/OpenGLShader.h
@@ -13,16 +13,16 @@ namespace Fusion { namespace Graphics {
 		virtual void Bind() override;
 		virtual void Unbind() override;
 
-		virtual void SetInt(const std::string& name, const int value) override;
-		virtual void SetIntArray(const std::string& name, const int* const values, const uint32_t count) override;
-		virtual void SetFloat(const std::string& name, float value) override;
-		virtual void SetFloat2(const std::string& name, const glm::vec2& value) override;
-		virtual void SetFloat3(const std::string& name, const glm::vec3& value) override;
-		virtual void SetFloat4(const std::string& name, const glm::vec4& value) override;
-		virtual void SetMat4(const std::string& name, const glm::mat4& value) override;
+		virtual void SetInt(const char* name, const int value) override;
+		virtual void SetIntArray(const char* name, const int* const values, const uint32_t count) override;
+		virtual void SetFloat(const char* name, float value) override;
+		virtual void SetFloat2(const char* name, const glm::vec2& value) override;
+		virtual void SetFloat3(const char* name, const glm::vec3& value) override;
+		virtual void SetFloat4(const char* name, const glm::vec4& value) override;
+		virtual void SetMat4(const char* name, const glm::mat4& value) override;
 	private:
 		uint32_t CompileShader(const char* source, const uint32_t type);
-		int GetUniformLocation(const std::string& name);
+		int GetUniformLocation(const char* name);
 	private:
 		uint32_t m_Program;
 	};
